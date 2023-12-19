0025de007b42e964bc13d0d2fb41cb2c48da8c0c
Jones Nambundo <131800544+jonesnambundo@users.noreply.github.com>
2023-12-18 22:38:16 -0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,5 @@
 AOS.init();
-
-const dataDoEvento = new Date("Dec 12, 2023 19:00:00");
+const dataDoEvento = new Date("April 12, 2024 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function() {
